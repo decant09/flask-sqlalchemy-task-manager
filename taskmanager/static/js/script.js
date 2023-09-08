8f9342f098ce00d3fa992d3a13b952867bdad31d
decant09 <>
2023-09-08 10:42:18 +0000

    Add ability to view tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
@@ -7,11 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
     let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
         format: "dd mmmm, yyyy",
-        i18n: { done: "Select" }
+        i18n: {done: "Select"}
     });
 
     // select initialization
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
-});
 
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
+});
